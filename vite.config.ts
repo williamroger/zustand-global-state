728a1eb6494eb602dfef8950f94349b9f2bd6938
William Roger <williamroger.frontend@gmail.com>
2026-01-26 16:55:18 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    open: true,
+  },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
